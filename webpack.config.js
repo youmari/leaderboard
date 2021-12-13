d740da71e49ee5b05bed0fe6796ecaf636095440
youmari <youmari300@gmail.com>
2021-12-13 22:41:54 +0100

    Set webpack and install dependencies

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { webpack } = require("webpack");
+
+module.exports = {
+    entry: './src/index.js',
+    mode: process.env.NODE_ENV === 'production' ? 'production ' : 'development',
+    module: {
+        rules:[
+            {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js)$/, 
+        use: 'babel-loader'
+      }
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
+}
